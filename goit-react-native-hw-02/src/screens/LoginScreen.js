4f95682e941e01ade17290801a89b0422da06d17
Olga-Melnyk <3717129@ukr.net>
2023-04-02 12:58:12 +0300

    fix

--- goit-react-native-hw-02/src/screens/LoginScreen.js
+++ goit-react-native-hw-02/src/screens/LoginScreen.js
@@ -8,7 +8,7 @@ import {
   TouchableOpacity,
 } from "react-native";
 import * as Font from "expo-font";
-import { AppLoading } from "expo";
+import AppLoading from "expo-app-loading";
 
 const loadApplicationLogin = async () => {
   await Font.loadAsync({
